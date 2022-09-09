455c85f32e5610f1f5a04bbe2039473de83635f4
Abhilash Patel <81802634+anonymousabhi13@users.noreply.github.com>
2022-09-09 16:03:37 +0530

    Update Configuration.js

--- src/Components/Utility/Configuration.js
+++ src/Components/Utility/Configuration.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAZwsglGTZ0QMrxpx4hVkQRt-AqGhspVS0",
-  authDomain: "dotted-aileron-345204.firebaseapp.com",
-  projectId: "dotted-aileron-345204",
-  storageBucket: "dotted-aileron-345204.appspot.com",
-  messagingSenderId: "650020210896",
-  appId: "1:650020210896:web:54b7d048b250af0bedb6d5",
-  measurementId: "G-7RLCCXY5VM"
+  apiKey: "",
+  authDomain: "",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 const app = initializeApp(firebaseConfig);
